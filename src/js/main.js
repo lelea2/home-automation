c27632707ca585eb44e80d5c5abcaca72825bcd8
kdao <khanh.dao@move.com>
2017-02-06 10:09:01 -0800

    Changing to modular pattern

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,37 @@
-var HOME_CONTROL = window.HOME_CONTROL || {};
+'use strict';
 
-/**
- * Function calling to init data
- */
-HOME_CONTROL.init = function() {
-  $.get('/datasource/home.json', function(data) {
-    // console.log(data);
-    var arr = [];
-    for (key in data) {
-      var obj = data[key];
-      var str = "<div class='" + key + "'><h4>" + obj.title + "</h4></div>";
-      arr.push(str);
-    }
-    var finalHTML = arr.join('');
-    $('#control-room-panel').html(finalHTML);
-  });
-};
+var HOME_CONTROL = (function($) {
+  /**
+   * Function calling to init data
+   */
+  function init() {
+    $.get('/datasource/home.json', function(data) {
+      // console.log(data);
+      var arr = [];
+      for (key in data) {
+        var obj = data[key];
+        var str = "<div class='" + key + "'><h4>" + obj.title + "</h4><ul>";
+        var lightOption = generateCheckBox();
+        var curtainOption = generateCheckBox();
+        var temperatureOption = generateCheckBox();
+        var str += lightOption + curtainOption + temperatureOption + "</ul></div>";
+        arr.push(str);
+      }
+      var finalHTML = arr.join('');
+      $('#control-room-panel').html(finalHTML);
+    });
+  }
+
+  function generateCheckBox() {
+
+  }
+
+  /**
+   * Return global function
+   */
+  return {
+    init: init
+  };
+
+}(jQuery));
 
