ab60e15d7a3d3c928670b100bdccb99f3cf45a91
kdao <khanh.dao@move.com>
2017-02-06 08:22:09 -0800

    Initial check in

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,19 @@
 var HOME_CONTROL = window.HOME_CONTROL || {};
 
+/**
+ * Function calling to init data
+ */
+HOME_CONTROL.init = function() {
+  $.get('/datasource/home.json', function(data) {
+    // console.log(data);
+    var arr = [];
+    for (key in data) {
+      var obj = data[key];
+      var str = "<div class='" + key + "'><h4>" + obj.title + "</h4></div>";
+      arr.push(str);
+    }
+    var finalHTML = arr.join('');
+    $('#control-room-panel').html(finalHTML);
+  });
+};
+
